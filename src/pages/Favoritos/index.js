35fca3be24e5d56ef3c2319ca67c07cecfded1d7
Eduardo-nascimento1 <eduardo1programador@hotmail.com>
2022-02-16 09:19:55 -0300

    Colocando funções na pagina favoritos,  adicionando função de exluir da lista e um botão para ver os detalhes do filme

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,7 +1,43 @@
+
+import { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom';
+import './favoritos.css';
+
 export default function Favoritos(){
-    return(
-        <div>
-            <h1> pagina filmes favoritos </h1>
-        </div>
-    )
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=>{
+
+    const minhaLista = localStorage.getItem('filmes');
+    setFilmes(JSON.parse(minhaLista) || []);
+
+  }, []);
+
+  function handledelete(id){
+      let filtrofilmes = filmes.filter((item)=>{
+          return(item.id!== id);
+      })
+      setFilmes(filtrofilmes)
+      localStorage.setItem('filmes',JSON.stringify(filtrofilmes))
+  }
+  
+  return(
+    <div id="meus-filmes">
+      <h1>Meus Filmes</h1>
+
+      <ul>
+        {filmes.map((item)=>{
+          return(
+            <li key={item.id}>
+              <span>{item.nome}</span>
+              <div>
+                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                <button onClick={()=>handledelete(item.id)}>Excluir</button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
